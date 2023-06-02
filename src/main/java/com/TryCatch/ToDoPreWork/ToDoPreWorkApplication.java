7d1a02ddd9a8e241c7a3da049ab41cfe0f476452
Javier Arley Sanchez Ledesma <javier.arley.sanchez.ledesma@gmail.com>
2023-06-02 02:48:32 -0500

    Primer Ejercicio con SprinBoot

---
+++ src/main/java/com/TryCatch/ToDoPreWork/ToDoPreWorkApplication.java
@@ -0,0 +1,13 @@
+package com.TryCatch.ToDoPreWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoPreWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoPreWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TryCatch/ToDoPreWork/ToDoPreWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TryCatch.ToDoPreWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoPreWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
